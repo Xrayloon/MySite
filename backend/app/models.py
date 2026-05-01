6b392db4b125de18bf289295bf38a0e4d3911179
Иван Воронин <thexrayloon@gmail.com>
2026-05-02 02:19:39 +0300

    Заглушки для классов

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,18 +1,11 @@
-from sqlalchemy import Table,MetaData, Column, Integer, String, ForeignKey
-#TO-DO: переместить сюда все модели таблицы и дополнять их 
+from pydantic import BaseModel 
 
-user_info = Table(
-    'user_info', MetaData,
-    Column('id', Integer(), primary_key=True),
-    Column('nickname', String(200), nullable=False),
-    Column('email',String(150), unique=True, nullable=False)
-)
 
-user_profile = Table(
-    'user_profile', MetaData,
-    Column('first_name', String(50), nullable=True),
-    Column('second_name',String(50), nullable=True),
-    Column('phone_nubmer',String(20), nullable=True, unique=True),
-    Column('user_id', ForeignKey('user_info.id')),
-)
+
+class User(BaseModel):
+    pass
+
+
+class register_user(User):
+    pass
 
